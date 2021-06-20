63d34930b7ad169731b0048a7f273af171b83017
nik-ford <57837812+nik-ford@users.noreply.github.com>
2021-06-21 01:31:59 +0200

    styled component grid
    added styled component grid

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '../assets/images/logopngblack.png'
 
+// TODO update this to use styled grid context
+
 const Navbar = () => {
 
 
