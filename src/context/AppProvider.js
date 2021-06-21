d45e053cb128809bcc47cc2f5c20619807bbcc20
nik-ford <57837812+nik-ford@users.noreply.github.com>
2021-06-21 16:33:33 +0200

    responsive grid
    added responsive prop and moved to Context

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,13 +1,42 @@
 import React, { createContext } from 'react'
-// import styled from 'styled-components' TODO REMOVE
+import styled from 'styled-components'
 
 export const AppContext = createContext({})
 
 export const AppProvider = ({ children }) => {
+
+const Grid = styled.div`
+border: 1px solid purple;
+`;
+const Row = styled.div`
+border: 1px solid violet;
+display: flex;
+`;
+const media = {
+	xs: (styles) => `
+	@media only screen and (max-width: 580px) {
+		${styles}
+	}
+	`,
+}
+
+const Col = styled.div`
+border: 1px solid violet;
+padding: 5px;
+flex: ${(props) => props.size};
+${(props) => props.collapse && media[props.collapse] (`
+display: none;
+`)};
+`;
+// RESPONSIVE DESIGN PASSED AS PROPS USING STRING INTERPOLATION
+// PASS other media queries add other properties to xs
+
     return (
         <AppContext.Provider
         value={{
-            
+            Grid,
+            Row,
+            Col
         }}
         >{children}
         </AppContext.Provider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,46 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from 'styled-components';
-import useWindowDimensions from '../hooks/useWindowDimension'
-import Navbar from '../components/Navbar'
+// import useWindowDimensions from '../hooks/useWindowDimension'
+// import Navbar from '../components/Navbar'
+import {AppContext} from '../context/AppProvider'
 
 // TODO move styled component variables in a personal context
 // I will leverage styled components to dinamically render mobile/desktop styled component
 // I can use ternary expressions or props, I'll decide later, 
 // now it's too late.......it's time to build a GIN TONIC, chill, and then go bed :)
 
-export const Grid = styled.div`
-border: 1px solid purple;
-`;
-export const Row = styled.div`
-border: 1px solid violet;
-display: flex;
-`;
-export const Col = styled.div`
-border: 1px solid violet;
-flex: ${(props) => props.size}; // TODO create other props (THEME??)
-padding: 5px;
-`;
+
 export const Dimensions = styled.h1`
 text-align: center;
 `;
 
 const Home = () => {
-	const { height, width } = useWindowDimensions();
-
+	const {Grid, Row, Col } = useContext(AppContext)
 
   return (
 		<Grid>
 			<Row>
 				<Col size={1}>
-					full size column copyPasteMe!
+					Navbar
+				</Col>
+			</Row>
+			<Row>
+				<Col size={1}>
+					hero header
+				</Col>
+			</Row>
+			<Row>
+				<Col size={1}>
+					subsection
 				</Col>
 			</Row>
 			<Row>
 				<Col size={2}>
-					article section size
+					article section
 				</Col>
-				<Col size={1}>
+				<Col
+				 size={1}
+				 collapse={'xs'}>
 					footer
 				</Col>
 			</Row>
